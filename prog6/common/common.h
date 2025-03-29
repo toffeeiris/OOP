066c908633dd24a42f84a02a32a5e121207bd42e
toffeeiris <toffeeiris@yahoo.com>
2025-03-29 13:29:59 +0300

    Add files via upload
    Separation of work with polynomials on sets of real and complex numbers.

--- prog6/common/common.h
+++ prog6/common/common.h
@@ -1,1 +1,28 @@
+#ifndef COMMON_H
+#define COMMON_H
 
+#include <QString>
+
+enum messages
+{
+    VALUE_REQUEST = 1,
+    VALUE_ANSWER,
+    PRINT_CLASSIC_REQUEST,
+    PRINT_CANONIC_REQUEST,
+    PRINT_ANSWER,
+    UPDATE_REQUEST,
+    CHANGE_ROOT_REQUEST,
+    END_REQUEST,
+    END_ANSWER
+};
+
+enum modes
+{
+    D_MODE = 1,
+    C_MODE,
+};
+
+extern const QChar separator;
+QString& operator<< (QString&,const QString&);
+
+#endif // COMMON_H
