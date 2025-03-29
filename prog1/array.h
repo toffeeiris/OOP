c26e2d799ba1c6485bd5e9840d6a62689d1dce41
toffeeiris <toffeeiris@yahoo.com>
2025-03-29 11:40:01 +0300

    Add files via upload
    Development of an arbitrary-length Array class, capable of storing objects of the abstract number type.

---
+++ prog1/array.h
@@ -0,0 +1,39 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <iostream>
+#include <ostream>
+
+#include "number.h"
+
+using namespace std;
+
+class TArray
+{
+
+    number *arr;
+    int size, point = 0;
+
+public:
+    TArray();
+    TArray(int);
+
+    ~TArray();
+
+    friend istream& operator>> (istream&, TArray&);
+    friend ostream& operator<< (ostream&, TArray&);
+
+    void resize(int new_size);
+    number avarage_value();
+    number standard_deviation();
+    void sort(bool);
+    void change_index(int, number);
+    void clear_arr();
+
+    int get_size()
+    {
+        return size;
+    }
+};
+
+#endif // ARRAY_H
